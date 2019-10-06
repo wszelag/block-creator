3716231586dd16ae51a11895329199edc49207ed
Wojtek Szelag <wojciech2008@wp.pl>
2019-10-06 17:56:27 +0200

    add creator feature

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-//scene
+//<<--settings -->>
+//<-scene
 const scene = new THREE.Scene();
 const bgColor = new THREE.Color("rgb(230, 199, 142)");
 scene.background = bgColor;
-//camera
+//<-camera
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
-  0.1,
+  0.5,
   1000
 );
-camera.position.z = 5;
-//renderer
+camera.position.z = 30;
+//<-renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-//elements creator
-const creator = (
-  sizeX = 1,
-  sizeY = 1,
-  sizeZ = 1,
-  posX = 0,
-  posY = 0,
-  posZ = 0
-) => {
+
+//<<--block creator function -->>
+const creator = () => {
+  const sizeX = Math.random() * 5;
+  const sizeY = Math.random() * 5;
+  const sizeZ = Math.random() * 5;
+  const posX = Math.random() * 60 - 30;
+  const posY = Math.random() * 20 - 10;
+  const posZ = Math.random() * 20 - 10;
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
   const geometry = new THREE.BoxGeometry(sizeX, sizeY, sizeZ);
-  const material = new THREE.MeshBasicMaterial({ color: "rgb(38, 136, 240)" });
+  const material = new THREE.MeshBasicMaterial({
+    color: `rgb(${red}, ${green}, ${blue})`
+  });
   const element = new THREE.Mesh(geometry, material);
   element.position.x = posX;
   element.position.y = posY;
@@ -34,6 +41,7 @@ const creator = (
   scene.add(element);
 };
 
+//<<--init, events-->>
 const animate = () => {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
@@ -41,4 +49,6 @@ const animate = () => {
 
 animate();
 
-document.querySelector("button").addEventListener("click", () => creator());
+document.querySelector("button").addEventListener("click", () => {
+  creator();
+});
